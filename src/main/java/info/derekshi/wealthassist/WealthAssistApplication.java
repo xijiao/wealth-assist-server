7223ceb3b1c02659cfa3bfbd0b8546163c603853
xijiao <xijiaosdq@gmail.com>
2021-09-19 10:24:47 +0900

    initial commit

---
+++ src/main/java/info/derekshi/wealthassist/WealthAssistApplication.java
@@ -0,0 +1,13 @@
+package info.derekshi.wealthassist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WealthAssistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WealthAssistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/info/derekshi/wealthassist/WealthAssistApplicationTests.java
@@ -0,0 +1,13 @@
+package info.derekshi.wealthassist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WealthAssistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
