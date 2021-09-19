3f6c7dede985181fd13ec60d18740e4cac446fba
xijiao <xijiaosdq@gmail.com>
2021-09-19 16:36:22 +0900

    Build configs.

--- src/main/java/info/derekshi/wealthassist/WealthAssistApplication.java
+++ src/main/java/info/derekshi/wealthassist/WealthAssistApplication.java
@@ -2,12 +2,22 @@ package info.derekshi.wealthassist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@Controller
 public class WealthAssistApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WealthAssistApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public @ResponseBody ResponseEntity<String> get() {
+		return new ResponseEntity<String>("GET Response", HttpStatus.OK);
+	}
 }
