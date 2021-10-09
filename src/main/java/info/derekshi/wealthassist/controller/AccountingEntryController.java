a3782c08c68400a9e74f470471b24d2757f7d2a1
xijiao <xijiaosdq@gmail.com>
2021-10-09 15:37:17 +0900

    Bug fixes.

--- src/main/java/info/derekshi/wealthassist/controller/AccountingEntryController.java
+++ src/main/java/info/derekshi/wealthassist/controller/AccountingEntryController.java
@@ -1,16 +1,21 @@
 package info.derekshi.wealthassist.controller;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import info.derekshi.wealthassist.WealthAssistApplication;
 import info.derekshi.wealthassist.model.AccountingEntry;
@@ -37,6 +42,22 @@ public class AccountingEntryController {
   @PutMapping("{userId}/entry")
   public AccountingEntry putEntry(@RequestBody AccountingEntry entry, @PathVariable Long userId) {
     log.info(entry.toString());
-    return entryRepository.save(entry);
+    if (entry.getId() == null) {
+      entry.setCreateDate(new Timestamp(new Date().getTime()));
+      entry.setModifyDate(new Timestamp(new Date().getTime()));
+      entry.setUserId(userId);
+      return entryRepository.save(entry);
+    } else {
+      Optional<AccountingEntry> dbEntry = entryRepository.findById(entry.getId());
+      if (dbEntry.isPresent()) {
+        if (entry.getUserId() != dbEntry.get().getUserId()) {
+          throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Not your Entry!");
+        }
+        entry.setCreateDate(dbEntry.get().getCreateDate());
+        entry.setRevision(dbEntry.get().getRevision() + 1);
+      }
+      entry.setModifyDate(new Timestamp(new Date().getTime()));
+      return entryRepository.save(entry);
+    }
   }
 }
